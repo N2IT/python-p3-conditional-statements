e510621272fc106272f00a2b22d0a91f569b9717
Tony Eder <tonyeder11@gmail.com>
2024-02-01 10:24:51 -0800

    all passed

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -46,4 +46,16 @@ def fizzbuzz(num):
 
 def calculator(operation, num1, num2):
     # your code here
-    pass
+    # if operation is one of +, -, *, / return value of operation of num1 and num2
+    # else return 'Invalid operation'
+    if operation == '+':
+        return num1 + num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == '/':
+        return num1 / num2
+    else:
+        print('Invalid operation!')
+        return None
