d21014788c44d503eb1dd6d4a51bc8fba5067f1f
Tony Eder <tonyeder11@gmail.com>
2024-02-01 09:50:50 -0800

    first test passed

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,8 +1,18 @@
+
 #!/usr/bin/env python3
 
 def admin_login(username, password):
     # your code here
-    pass
+    # username needs to equal admin OR admin
+    # password needs to equal 12345
+    # all conditions need to be met to return "Access granted" else return "Access denied"
+    username = username.lower()
+    # if username == "admin" and password == "12345":
+    #     return "Access granted"
+    # else:
+    #     return "Access denied"
+
+    return "Access granted" if username == "admin" and password == "12345" else "Access denied"
 
 def hows_the_weather(temperature):
     # your code here
